9873559beddb3a2542133aa9edadbbbe83f6054f
mateuskuritza <mateuskuritza@gmail.com>
2022-12-15 13:41:58 -0300

    add validator

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { randomUUID } from 'crypto';
+import { CreateNotificationBody } from './create-notification-body';
 import { PrismaService } from './database/prisma.service';
 
 @Controller('notifications')
@@ -12,13 +13,15 @@ export class AppController {
   }
 
   @Post()
-  create() {
+  create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+
     return this.prisma.notification.create({
       data: {
         id: randomUUID(),
-        content: 'Você tem uma nova solicitação de amizade!',
-        category: 'social',
-        recipientId: randomUUID(),
+        content,
+        category,
+        recipientId,
       },
     });
   }
