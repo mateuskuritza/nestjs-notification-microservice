236f4ac77e680c31586e0fddc9031f7a4a8d0172
mateuskuritza <mateuskuritza@gmail.com>
2022-12-16 19:32:40 -0300

    implements prisma service methods and "inverse" mapper

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,5 +1,6 @@
+import { Content } from '@app/entities/content';
 import { Notification } from '@app/entities/notification';
-
+import { Notification as RawNotification } from '@prisma/client';
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
     return {
@@ -10,5 +11,19 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toApp(raw: RawNotification): Notification {
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+        readAt: raw.readAt,
+      },
+      raw.id,
+    );
   }
 }
